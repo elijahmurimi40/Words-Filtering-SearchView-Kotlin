d603afdf309c3557b8bcc6169ac4203803534359
Fortie40 <elijahmurimi40@gmail.com>
2020-06-17 12:09:25 +0300

    Initial commit

---
+++ app/src/main/java/com/fortie40/wordsfiltering/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fortie40.wordsfiltering
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
