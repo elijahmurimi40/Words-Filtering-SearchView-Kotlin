9ae60cc7b0fa68548c1dfd874ab3e12644a7cd0d
Fortie40 <elijahmurimi40@gmail.com>
2020-06-18 19:32:05 +0300

    Add search functionality

--- app/src/main/java/com/fortie40/wordsfiltering/MainActivity.kt
+++ app/src/main/java/com/fortie40/wordsfiltering/MainActivity.kt
@@ -1,15 +1,57 @@
 package com.fortie40.wordsfiltering
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var searchView: SearchView
+    private lateinit var adapter: MainActivityAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getNames()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        searchView = menu!!.findItem(R.id.app_bar_search).actionView as SearchView
+        searchView.imeOptions = EditorInfo.IME_FLAG_NO_EXTRACT_UI
+        searchView.maxWidth = Integer.MAX_VALUE
+        searchView.queryHint = getString(R.string.search_name)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchName(query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchName(newText)
+                return false
+            }
+        })
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onBackPressed() {
+        if (!searchView.isIconified) {
+            searchView.setQuery("", false)
+            searchView.clearFocus()
+            searchView.isIconified = true
+        } else {
+            super.onBackPressed()
+        }
     }
 
     private fun getNames() {
@@ -21,7 +63,21 @@ class MainActivity : AppCompatActivity() {
             "Hollywood", "SMALL", "Lisp", "PureScript", "R++", "XQuery", "YAML", "ZOPL"
         )
 
-        val adapter = MainActivityAdapter(names)
+        adapter = MainActivityAdapter(names)
         names_item.adapter = adapter
+    }
+
+    private fun searchName(p0: String?) {
+        adapter.filter.filter(p0!!.toLowerCase(Locale.getDefault())) {
+            when(adapter.itemCount) {
+                0 -> {
+                    no_results_found.visibility = View.VISIBLE
+                    no_results_found.text =getString(R.string.no_results_found, p0)
+                }
+                else -> {
+                    no_results_found.visibility = View.GONE
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/fortie40/wordsfiltering/MainActivityAdapter.kt
+++ app/src/main/java/com/fortie40/wordsfiltering/MainActivityAdapter.kt
@@ -3,11 +3,17 @@ package com.fortie40.wordsfiltering
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
-class MainActivityAdapter(private val names: List<String>):
-    RecyclerView.Adapter<MainActivityAdapter.MainActivityViewHolder>() {
+class MainActivityAdapter(names: List<String>):
+    RecyclerView.Adapter<MainActivityAdapter.MainActivityViewHolder>(), Filterable {
+
+    private var originalList: List<String> = names
+    private var mFilteredList: List<String> = names
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainActivityViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -16,12 +22,40 @@ class MainActivityAdapter(private val names: List<String>):
     }
 
     override fun getItemCount(): Int {
-        return names.size
+        return mFilteredList.size
     }
 
     override fun onBindViewHolder(holder: MainActivityViewHolder, position: Int) {
-        val name = names[position]
+        val name = mFilteredList[position]
         holder.bind(name)
+    }
+
+    override fun getFilter(): Filter {
+        return this.filter
+    }
+
+    private val filter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            val chaString = constraint.toString()
+
+            mFilteredList = if (chaString.isEmpty()) {
+                originalList
+            } else {
+                val filteredList = originalList
+                    .filter { it.toLowerCase(Locale.getDefault()).contains(chaString) }
+                    .toMutableList()
+                filteredList
+            }
+            val filterResults = FilterResults()
+            filterResults.values = mFilteredList
+            return filterResults
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            mFilteredList = results!!.values as List<String>
+            notifyDataSetChanged()
+        }
     }
 
     class MainActivityViewHolder(private val nItemView: View): RecyclerView.ViewHolder(nItemView) {
